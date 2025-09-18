8a5e62f18883bc4cb790f0a52fb3b97332469b48
Sofigypalo <s.giroldisommer@colegiocalasanz.edu.ar>
2025-09-18 15:21:28 -0300

    Add files via upload

---
+++ ejercicio1.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById ("boton")
+let parrafo = document. getElementById ("texto")
+btn.addEventLister ("click, function"()¨{ 
+    parrafo.inerHTML="Sofig";
+
+
+    
+});
